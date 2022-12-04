7051a62cdc84956f6ad6f24bafeb3eb3f2915a5c
Rezuan Talukder <rez-tal@outlook.com>
2022-12-04 04:10:19 +0000

    Add validators to thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,16 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=300)
-    quantity = models.IntegerField()
+    name = models.CharField(
+        unique = True,
+        blank = False,
+        max_length=30
+    )
+
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(validators= [
+        MinValueValidator(1, "Value cannot be more than 0"),
+        MinValueValidator(100, "Value cannot be less than 100"),
+    ])
